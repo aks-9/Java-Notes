eac6ba2d8629b20da506719fc7afa76ed6dda402
Ashish Kumar Singh <ashishsingh4job@gmail.com>
2022-11-24 00:18:30 +0530

    Add files via upload

---
+++ 11. Practice Set -2.java
@@ -0,0 +1,43 @@
+// Practice set -2 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        // 1. What will be the result of the following expression: float a = 7/4 * 9/2
+//        float a = 7/4.0f * 9/2.0f;
+//        System.out.println(a);
+
+        // 2. Write a java program to encrypt a grade by adding 8 to it. Decrypt it to show the correct grade.
+
+        // Encrypting
+//        char grade = 'B';
+//        grade = (char)(grade + 8);
+//        System.out.println(grade);
+
+        // Decrypting
+//        grade = (char)(grade - 8);
+//        System.out.println(grade);
+
+        // 3. Use comparison operators to find out whether a given number is greater than the user entered number or not.
+//        int b = 5;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter a number: ");
+//        int c = sc.nextInt();
+//        System.out.println("Entered number is greater than 5? :" + (c>b));
+
+        // 4. Write the following expression in a java program: (v^2-u^2)/2as
+//        float v=5, u=3, a= 1, s= 1;
+//        float expression = v * v -  u * u / 2 * a * s;
+//        // 25 - 9/2
+//        // 25 -4.5
+//        // 20.5
+//        System.out.println(expression);
+
+//        5. Find the value of 'a' in expression: int a = 7*49/7 + 35/7
+        // 54
+
+
+    }
+}
