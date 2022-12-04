6e88403bae21bc2af60ae169a651784c1cf24482
Ashish Kumar Singh <ashishsingh4job@gmail.com>
2022-12-04 19:49:52 +0530

    Add files via upload

---
+++ 55. Create Packages.java
@@ -0,0 +1,32 @@
+//* Creating Packages in Java
+
+/*
+
+How to create a package in Java :
+
+javac -d Harry java 
+
+The above code creates a packages folder.
+
+java Harry java 
+
+The above code creates Harry class.
+
+    We can also create inner packages by adding packages inner as the package name.
+    These packages once created can be used by other classes.
+
+*/
+
+package com.company;
+
+import java.util.Scanner;
+//import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("This is my scanner taking input as " + a);
+    }
+}
+ 
