5a15d808dc03f7ed3122702cf403256a35122d51
Ashish Kumar Singh <ashishsingh4job@gmail.com>
2022-12-06 22:39:28 +0530

    Add files via upload

---
+++ 58. Exercise 5: Creating a Custom Package.java
@@ -0,0 +1,22 @@
+//* Java Exercise 5: Creating a Custom Package
+
+/*
+
+ You have to create a package named com.codewithharry.shape
+ This package should have individual classes for Rectangle, Square, Circle, Cylinder, Sphere
+ These classes should use inheritance to properly manage the code!
+ Include methods like volume, surface area and getters/setters for dimensions
+
+*/
+
+package com.company;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+ 
