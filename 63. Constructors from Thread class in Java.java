9ada3493a8278d59f6ec9d2618d478b68a1b4c47
Ashish Kumar Singh <ashishsingh4job@gmail.com>
2022-12-10 19:47:28 +0530

    Add files via upload

---
+++ 63. Constructors from Thread class in Java.java
@@ -0,0 +1,44 @@
+//* Constructors from Thread class in Java 
+
+/*
+The Thread class 
+Below are the commonly used constructors of the thread class: 
+
+1. Thread ( ) 
+2. Thread ( string ) 
+3. Thread ( Runnable r )
+4. Thread ( Runnable r, String name ) 
+
+*/
+
+package com.company;
+
+class MyThr extends Thread{
+    public MyThr(String name){
+        super(name);
+    }
+    public void run(){
+        int i = 34;
+        System.out.println("Thank you");
+//        while(true){
+//            System.out.println("I am a thread");
+//        }
+    }
+}
+public class cwh_73_thread_constructor {
+    public static void main(String[] args) {
+    MyThr t1 = new MyThr("Harry");
+    MyThr t2 = new MyThr("Ram Candr");
+    t1.start();
+    t2.start();
+    System.out.println("The id of the thread t is " + t1.getId());
+    System.out.println("The name of the thread t is " + t1.getName());
+    System.out.println("The id of the thread t is " + t2.getId());
+    System.out.println("The name of the thread t is " + t2.getName());
+
+    }
+}
+
+
+
+ 
